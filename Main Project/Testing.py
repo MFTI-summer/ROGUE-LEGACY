f63e8f7ad61cde2599d71511bf1ac53be3f9ac94
haryuuno21 <d.shilenok@mail.ru>
2021-06-30 20:27:27 +0300

    Update Testing.py

--- Main Project/Testing.py
+++ Main Project/Testing.py
@@ -7,7 +7,7 @@ import UI
 
 if __name__ == '__main__':
 
-    MANA_RESTORED_FOR_TICK = 2
+    MANA_RESTORED_FOR_TICK = 5
 
     WIN_width = 750
     WIN_height = 500
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     mana_potion = Item.RestoreManaItem(50)
     hero = Hero.Hero(x=300, y=300)
     ui = UI.UI(hero, 5, WIN_height-5)
-    for _ in range(9):
+    for _ in range(10):
         ui.heal_slot.store_item(heal_potion)
         ui.mana_slot.store_item(mana_potion)
     # hero.rect.x = 300
