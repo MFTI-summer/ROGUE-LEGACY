85ebd41cd46bf059e6adc702cd1958368958ed17
haryuuno21 <d.shilenok@mail.ru>
2021-06-30 13:54:03 +0300

    Update Inventory.py

--- Main Project/Inventory.py
+++ Main Project/Inventory.py
@@ -1,9 +1,12 @@
+import pygame
+
+
 class InventorySlot:
     def __init__(self, length, hero, type):
-        self._length = length #Максимальная вместимость слота
+        self._length = length  # Максимальная вместимость слота
         self._buf = []  # Предметы в слоте
         self._hero = hero
-        self._type = type #Тип предмета
+        self._type = type  # Тип предмета
 
     def store_item(self, item):
         if self._type == item.get_type():
@@ -25,8 +28,12 @@ class InventorySlot:
         return self._length
 
 
-class Item:
-    def __init__(self, type):
+class Item(pygame.sprite.Sprite):
+    def __init__(self, type, sprite_path, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(sprite_path)
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (x, y)
         self._type = type
 
     def get_type(self):
@@ -34,8 +41,8 @@ class Item:
 
 
 class RestoreManaItem(Item):
-    def __init__(self, mana):
-        super().__init__("MANA")
+    def __init__(self, mana, x, y):
+        super().__init__("MANA", "Textures/mana potion.png", x, y)
         self._mana = mana
 
     def use(self, hero):
@@ -43,9 +50,9 @@ class RestoreManaItem(Item):
 
 
 class HealItem(Item):
-    def __init__(self, heal):
-        super().__init__("HP")
-        self._heal = heal  #Сколько восстановит
+    def __init__(self, heal, x, y):
+        super().__init__("HP", "Textures/healing salve.png", x, y)
+        self._heal = heal  # Сколько восстановит
 
     def use(self, hero):
         hero.heal(self._heal)
