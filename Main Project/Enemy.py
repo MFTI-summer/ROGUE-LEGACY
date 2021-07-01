6f65b04f3cb202847ed6cd8505d0063816cc57d9
Aleksandr <mozhuhin71@gmail.com>
2021-07-01 15:21:59 +0300

    .

--- Main Project/Enemy.py
+++ Main Project/Enemy.py
@@ -1,4 +1,9 @@
 import pygame
+
+window_widht=1000
+window_height=500
+
+
 class Enemy():
     walkRight = [pygame.image.load('Animations/Enemy/R1E.png'), pygame.image.load('Animations/Enemy/R2E.png'), pygame.image.load('Animations/Enemy/R3E.png'), pygame.image.load('Animations/Enemy/R4E.png'), pygame.image.load('Animations/Enemy/R5E.png'), pygame.image.load('Animations/Enemy/R6E.png'), pygame.image.load('Animations/Enemy/R7E.png'), pygame.image.load('Animations/Enemy/R8E.png'), pygame.image.load('Animations/Enemy/R9E.png'), pygame.image.load('Animations/Enemy/R10E.png'), pygame.image.load('Animations/Enemy/R11E.png')]
     walkLeft = [pygame.image.load('Animations/Enemy/L1E.png'), pygame.image.load('Animations/Enemy/L2E.png'), pygame.image.load('Animations/Enemy/L3E.png'), pygame.image.load('Animations/Enemy/L4E.png'), pygame.image.load('Animations/Enemy/L5E.png'), pygame.image.load('Animations/Enemy/L6E.png'), pygame.image.load('Animations/Enemy/L7E.png'), pygame.image.load('Animations/Enemy/L8E.png'), pygame.image.load('Animations/Enemy/L9E.png'), pygame.image.load('Animations/Enemy/L10E.png'), pygame.image.load('Animations/Enemy/L11E.png')]
@@ -17,18 +22,19 @@ class Enemy():
 
     def draw(self,win):
         self.move()
-        if self.walkCount + 1 >= 33:
-            self.walkCount = 0
+        if self.isAlive:#отрисовка персонажа, при условии, что тот жив
+            if self.walkCount + 1 >= 33:
+                self.walkCount = 0
 
-        if self.vel > 0:
-            win.blit(self.walkRight[self.walkCount //3], (self.x, self.y))
-            self.walkCount += 1
-        else:
-            win.blit(self.walkLeft[self.walkCount //3], (self.x, self.y))
-            self.walkCount += 1
-        pygame.draw.rect(win, (255,0,0), self.hitbox,2)
+            if self.vel > 0:
+                win.blit(self.walkRight[self.walkCount //3], (self.x, self.y))
+                self.walkCount += 1
+            else:
+                win.blit(self.walkLeft[self.walkCount //3], (self.x, self.y))
+                self.walkCount += 1
 
-    def move(self):
+
+    def move(self):#движение персонажа по заданной траектории
         if self.vel > 0:
             if self.x + self.vel < self.path[1]:
                 self.x += self.vel
@@ -47,5 +53,23 @@ class Enemy():
             self.isAlive=False
         else:
             self.health-=gotten_damage
+    def attack(self):
+        pass
+
+def main():
+    display = pygame.display.set_mode((window_widht, window_height))
+    clock = pygame.time.Clock()
+    run = True
+    enemy=Enemy(20,250, 400)
+    while run:
+        display.fill([255] * 3)
+        enemy.draw(display)
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                return
+        clock.tick(60)
+        pygame.display.update()
+if __name__ == '__main__':
+    main()
 
 
